b0160424801a2d89ae845eb4517e5b143a9478be
Redhwan Ahmed <redhwan.ah1202@gmail.com>
2022-10-10 23:44:58 -0700

    finsihed some but has syntax errors

--- 482_Proj1/Project1.java
+++ 482_Proj1/Project1.java
@@ -55,12 +55,60 @@ public class Project1 {
 
         // Stable-Matching Algorithm:
         while(!stable && changes < 100) {
-            
+            stable = isStable(mensPreferences, womensPreferences, stableMatching );
             changes++; // Iterate the counter so the loop ends after 100 iterations.
         }
+        System.out.println(changes);
+        for(int i = 0; i < stableMatching.length; i++) {
+            System.out.println((i+1)+" "+stableMatching[i]);
+        }    
+
 
         
     }
+
+    public static boolean isStable(int[][] men, int[][] women, int[] matching) {
+        for(int i = 0; i < men.length; i++) { //int i can be used for men index on both matching and men arrays.
+            int manPartner = matching[i]; // finds the woman who man 'i' is currently with.
+            int manPartnerIndex = findIndex(men[i], manPartner); // Find the index of man 'i's partner.
+            for(int j = 0; j <= manPartnerIndex; j++) { // iterate through the mans array until the partner index is met.
+                
+                for(int k = 0; k < matching.length; k++) { // this is for checking the matches
+                    if(matching[k] == men[i][j]) { // this will find if the the woman in the matching array is the same in the men array.
+                        int currentPartnerW = findIndex(matching, matching[k]);
+                        int currentPartnerWIndex = findIndex(women[j], currentPartnerW);
+                        if(partnerExists(women[j], currentPartnerWIndex, manPartner)) {
+                            int temp = matching[manPartner-1]-1;
+                            matching[temp] = men[i][j];
+                            matching[currentPartnerW] = temp;
+                            return false;
+                        }
+                    }
+                }
+            }
+
+        }
+        return true;
+    }
+
+    // To find the index of a specific element in an array.
+    public static int findIndex(int[] list, int element) {
+        for(int i = 0; i < list.length; i++) {
+            if (list[i] == element)
+                return i;
+        }
+        return -1;
+    }
+
+    // To see if partner exists
+    public static boolean partnerExists(int[] list, int bound,  int person) {
+        for (int i = 0; i < bound; i++) {
+            if (list[i] == person) 
+                return true;
+        }
+        return false;
+    }
+
 
         // public static boolean checkStableMatchingEqual(int[][] arr1, int[][] arr2) {
         //     for(int i = 0; i < arr1.length; i++) {
